1da0cea744be999325d7aba44f70671786165fc1
Bijan Patel <bijan@binary.com>
2020-06-08 10:01:59 +0530

    Initial Repository

---
+++ src/main/java/com/qascript/utils/Common/PropertyFileUtils.java
@@ -0,0 +1,34 @@
+package com.qascript.utils.Common;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyFileUtils {
+
+    public static Properties loadApplicationProperties(){
+        Properties props = loadProperties("src/test/resources/config/application.properties");
+        return props;
+
+    }
+    public static Properties loadFrameworkProperties(){
+        Properties props = loadProperties("src/test/resources/config/framework.properties");
+        return props;
+    }
+
+    public static Properties loadProperties(String filePath){
+
+        Properties properties = new Properties();
+        try {
+            FileInputStream fs = new FileInputStream(filePath);
+            properties.load(fs);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+      return properties;
+
+    }
+}
